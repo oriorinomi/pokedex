3097008b01769f3803c6f2c8b514164962b1e1dc
AURELIA GUARINO <aureliaguarino0@gmail.com>
2024-10-24 21:36:18 +0200

    [FEAT]props

--- my-react-app/src/components/App.tsx
+++ my-react-app/src/components/App.tsx
@@ -1,9 +1,21 @@
 import PokemonCard from "./PokemonCard"
 
+const PokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
+
 function App () {
   return(
     <div> 
-    <PokemonCard/>
+    <PokemonCard pokemon={PokemonList[1]}/>
     </div>
   )
 }
--- my-react-app/src/components/PokemonCard.tsx
+++ my-react-app/src/components/PokemonCard.tsx
@@ -1,18 +1,5 @@
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
+function PokemonCard({ pokemon }) {
 
-function PokemonCard() {
-
-    const pokemon = pokemonList[0];
-    console.log(pokemon.imgSrc);
     return(
         
         
