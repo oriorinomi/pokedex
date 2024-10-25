116abcdda4591be4ef5d02ca2b0f5738e1c8d93a
AURELIA GUARINO <aureliaguarino0@gmail.com>
2024-10-25 11:05:34 +0200

    [FEAT]state pokedex

--- my-react-app/src/components/App.tsx
+++ my-react-app/src/components/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import PokemonCard from "./PokemonCard"
 
 const PokemonList = [
@@ -7,16 +8,47 @@ const PokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
     name: "mew",
   },
 ];
 
-
 function App () {
+
+  const handlePrecedentClick = () => {
+    setPokemonIndex (pokemonIndex > 0 ? pokemonIndex - 1 : PokemonList.length - 1);
+  }
+  
+  const handleSuivantClick = () => {
+    setPokemonIndex ((pokemonIndex + 1) % PokemonList.length);
+  }
+
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
   return(
+    <>
     <div> 
-    <PokemonCard name={PokemonList[1].name} imgSrc={PokemonList[1].imgSrc}/>
+    <PokemonCard name={PokemonList[pokemonIndex].name} imgSrc={PokemonList[pokemonIndex].imgSrc}/>
     </div>
+
+    <button onClick={handlePrecedentClick}>Précédent</button>
+    <button onClick={handleSuivantClick}>Suivant</button>
+
+    </>
   )
 }
 
